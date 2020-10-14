f9730595f40628f4b161150e712b3e6fc22bac9d
Mr.Robot <classic.graal095@gmail.com>
2020-10-14 18:32:12 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,9 @@
 module.exports = {
     pathPrefix: "/blogtim/"
 };
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('css')
+  return {
+    passthroughFileCopy: true
+  }
+}
